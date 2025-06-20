288d396c6f2c020ca4d47700c11df2f6f3318bfe
AnKiBaek <dksrlqor000804@gmail.com>
2025-06-20 17:30:51 +0900

    2025.06.20 안기백 개인작업 (진행중)

--- AKB/react_echocaine_AKB/src/api/api.ts
+++ AKB/react_echocaine_AKB/src/api/api.ts
@@ -3,8 +3,6 @@ import { instance } from "./instance";
 
 /**
  * 인기 영화 목록
- * @param page
- * @returns
  */
 export const apiGetMovieList = async (page: number) => {
   return await instance
@@ -13,9 +11,7 @@ export const apiGetMovieList = async (page: number) => {
 };
 
 /**
- * 키워드로 영화 검색 (페이지번호 포함)
- * @param search
- * @returns
+ * 키워드로 영화 검색
  */
 export const apiSearchMovieByKeyword = async (search: SearchMovieType) => {
   return await instance
@@ -24,9 +20,7 @@ export const apiSearchMovieByKeyword = async (search: SearchMovieType) => {
 };
 
 /**
- * 영화 정보
- * @param id
- * @returns
+ * 영화 상세 정보
  */
 export const apiMovieDetailInfo = async (id: string) => {
   return await instance.get(`movie/${id}`).then((res) => res.data);
@@ -34,35 +28,50 @@ export const apiMovieDetailInfo = async (id: string) => {
 
 /**
  * 영화 제작 정보
- * @param id
- * @returns
  */
 export const apiCreditInfo = async (id: string) => {
   return await instance.get(`/movie/${id}/credits`).then((res) => res.data);
 };
 
-// export const apiGetMusicList = async () => {
-//   return await instance
-//     .get(`search?offset=0&limit=20&query=BTS&type=artist`)
-//     .then((res) => res.data);
-// };
+/**
+ * 특정 아티스트 검색
+ */
+export const apiGetMusicList = async () => {
+  return await instance
+    .get(`search?offset=0&limit=20&query=BTS&type=artist`)
+    .then((res) => res.data);
+};
 
-// 입력한 장르의 아티스트 정보
+/**
+ * 입력한 장르의 아티스트 정보
+ */
 export const apiGetGenreArtist = async (genre: string) => {
   return await instance
     .get(`search?q=*&genre:${genre}&type=artist`)
     .then((res) => res.data);
 };
 
-// 입력한 장르의 트랙 정보
+/**
+ * 입력한 장르의 트랙 정보
+ */
 export const apiGetGenreTrack = async (genre: string) => {
   return await instance
     .get(`search?q=genre:${genre}&type=track`)
     .then((res) => res.data);
 };
 
-export const apiGetMusicList = async () => {
+/**
+ *최신 앨범 목록 가져오기
+ */
+export const apiGetNewReleases = async () => {
   return await instance
-    .get(`search?offset=0&limit=20&query=BTS&type=artist`)
+    .get(`browse/new-releases?limit=10`)
     .then((res) => res.data);
+};
+
+/**
+ * 특정 앨범의 트랙 목록 가져오기
+ */
+export const apiGetTracksByAlbumId = async (albumId: string) => {
+  return await instance.get(`albums/${albumId}/tracks`).then((res) => res.data);
 };
