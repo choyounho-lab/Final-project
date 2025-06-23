827d23586cc5244b6aef7e67564f805443d61be4
goms0000 <goms0000@naver.com>
2025-06-23 16:57:12 +0900

    수정

--- react/react_echocaine/src/pages/Home.tsx
+++ react/react_echocaine/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { apiGetNewReleases, apiGetTracksByAlbumId } from "../api/api";
 import { NewReleasesResponse, AlbumItem, Track } from "../types/types";
-import "../styles/Home.css";
+import "../assets/css/Home.css";
 
 function Home() {
   const [newReleasesData, setNewReleasesData] =
