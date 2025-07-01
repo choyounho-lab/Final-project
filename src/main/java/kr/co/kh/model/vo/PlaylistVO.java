dcaab5866794d3fa71ed5409589dfebeae665fd4
goms0000 <goms0000@naver.com>
2025-07-01 17:49:33 +0900

    PlaylistMapper, PlaylistVo

---
+++ src/main/java/kr/co/kh/model/vo/PlaylistVO.java
@@ -0,0 +1,20 @@
+package kr.co.kh.model.vo;
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Slf4j
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlaylistVO {
+    private Long playlistId;
+    private Long userId;
+    private String playlistTitle;
+    private Long playlistIsPublic;
+    private LocalDate playlistCreateDate;
+}
