4faf636fc90a92127e69c067d73acd6c7a8b28db
goms0000 <goms0000@naver.com>
2025-07-02 12:36:54 +0900

    Playlist 관련 파일 생성

---
+++ src/main/java/kr/co/kh/controller/playlist/PlaylistController.java
@@ -0,0 +1,43 @@
+package kr.co.kh.controller.playlist;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import kr.co.kh.annotation.CurrentUser;
+import kr.co.kh.model.CustomUserDetails;
+import kr.co.kh.model.payload.response.UserResponse;
+import kr.co.kh.model.vo.PlaylistVO;
+import kr.co.kh.service.PlaylistService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/playlist")
+@Slf4j
+@AllArgsConstructor
+public class PlaylistController {
+
+    private final PlaylistService playlistService;
+
+
+
+
+
+    @PostMapping("/create")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN') or hasRole('SYSTEM')")
+    @ApiOperation(value = "개인 플레이리스트 생성")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "currentUser", value = "사용자 정보", dataType = "CustomUserDetails", dataTypeClass = CustomUserDetails.class, required = true),
+            @ApiImplicitParam(name = "playlist", value = "플레이리스트", dataType = "PlaylistVO", dataTypeClass = PlaylistVO.class, required = true)
+    })
+    public ResponseEntity<?> getUserProfile(@CurrentUser CustomUserDetails currentUser, @RequestBody PlaylistVO playlistVO) {
+        UserResponse userResponse = new UserResponse(currentUser.getUsername(), currentUser.getEmail(), currentUser.getRoles());
+
+        playlistVO.setUserId(currentUser.getId());
+        playlistService.insertPlaylist(playlistVO);
+        return ResponseEntity.ok().build();
+    }
+}
