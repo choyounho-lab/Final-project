fffa7383b27b8144c762df7fa0ff133edd6873d7
choyounho-lab <choyh8@naver.com>
2025-07-11 17:34:31 +0900

    0711 조윤호 성별 생년월일 롤 값 정리 하였고 아이디 찾기 기능도 구현하였습니다

--- src/main/java/kr/co/kh/service/UserAuthorityService.java
+++ src/main/java/kr/co/kh/service/UserAuthorityService.java
@@ -1,10 +1,14 @@
 package kr.co.kh.service;
 
 import kr.co.kh.mapper.UserAuthorityMapper;
+import kr.co.kh.model.User;
+import kr.co.kh.model.payload.request.RegistrationRequest;
 import kr.co.kh.model.vo.UserAuthorityVO;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 @AllArgsConstructor
@@ -17,4 +21,15 @@ public class UserAuthorityService {
         userAuthorityMapper.save(userAuthorityVO);
     }
 
+    public void insertUser(User request){
+        userAuthorityMapper.insertUser(request);
+    }
+
+    public HashMap<String, Object> findByNameAndBirthDate(HashMap<String, Object> map) {
+        return userAuthorityMapper.findByNameAndBirthDate(map);
+    }
+
+    public User findByNameAndEmail(String name, String email) {
+        return userAuthorityMapper.findByNameAndEmail(name, email);
+    }
 }
