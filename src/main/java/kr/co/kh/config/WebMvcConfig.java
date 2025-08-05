47fbc764c2698d16b3d886705f1a6bafd3bc1703
goms0000 <goms0000@naver.com>
2025-08-05 10:35:09 +0900

    ,

--- src/main/java/kr/co/kh/config/WebMvcConfig.java
+++ src/main/java/kr/co/kh/config/WebMvcConfig.java
@@ -23,7 +23,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8000")
+                .allowedOrigins("*")
                 .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowCredentials(true)
                 .maxAge(MAX_AGE_SECS);
